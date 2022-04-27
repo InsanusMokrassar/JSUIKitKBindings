6daa57fe8f2b9128364c6fa64f88ec514f06be81
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-04-27 16:13:58 +0600

    improve support of List

--- src/jsMain/kotlin/dev/inmo/jsuikit/elements/List.kt
+++ src/jsMain/kotlin/dev/inmo/jsuikit/elements/List.kt
@@ -2,11 +2,29 @@ package dev.inmo.jsuikit.elements
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import dev.inmo.jsuikit.modifiers.UIKitModifier
-import dev.inmo.jsuikit.modifiers.include
+import dev.inmo.jsuikit.modifiers.*
+import dev.inmo.jsuikit.utils.Attrs
 import org.jetbrains.compose.web.dom.*
 import org.w3c.dom.HTMLHeadingElement
 import org.w3c.dom.HTMLUListElement
+
+@Composable
+fun <T> List(
+    data: SnapshotStateList<T>,
+    ukAttrs: Attrs<HTMLUListElement> = Attrs.empty(),
+    elementAllocator: @Composable ElementScope<HTMLUListElement>.(T) -> Unit
+) {
+    Ul(
+        {
+            include(UIKitList)
+            ukAttrs.builder(this)
+        }
+    ) {
+        data.forEach {
+            elementAllocator(it)
+        }
+    }
+}
 
 @Composable
 fun <T> List(
@@ -23,15 +41,5 @@ fun <T> List(
         Text(title)
     }
     besidesTitleAndList ?.invoke()
-    Ul(
-        {
-            classes("uk-list")
-            include(*ulModifiers)
-            ulCustomizer()
-        }
-    ) {
-        data.forEach {
-            elementAllocator(it)
-        }
-    }
+    List(data, Attrs(*ulModifiers) { ulCustomizer(this) }, elementAllocator)
 }
